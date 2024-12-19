27731975b15332136c547512b9eff01ae3497c09
Ahmed Aisha Beverly <aishabevy@gmail.com>
2024-12-20 00:32:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//  create a variable using first name
+let firstName = "Aisha";
+// create a second variable using middle name
+let middleName = "Beveerly";
+// let first name equal middle name
+firstName = middleName;
+// print the middle name
+console.log(middleName);
